cbe4459b86fb7b401eb327d53da75d12bdc1deaa
chrisbrody <chrisbrody@codifyacademy.com>
2016-08-18 13:39:56 -0700

    updated data to be displayed from JavaScript

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,35 @@
 /* GET home page */
 module.exports.homelist = function(req, res) {
-	res.render('index', { 
+	res.render('locations-list', { 
 		title : 'Loc8r - find a place to work with wifi',
 		pageHeader : {
 			title : 'Loc8r',
 			strapline : 'Find places to work with wifi near you!'
-		} 
+		},
+		sidebar : 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about.  Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.',
+		locations : [
+			{
+				name : 'Starbucks',
+				address : '125 High Street, San Francisco, CA 94105',
+				rating : 3,
+				facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+				distance : '100m'
+			},
+			{
+				name : 'Cafe Hero',
+				address : '335 High Street, San Francisco, CA 94105',
+				rating : 4,
+				facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+				distance : '200m'
+			},
+			{
+				name : 'Burger King',
+				address : '515 High Street, San Francisco, CA 94105',
+				rating : 2,
+				facilities : ['Food', 'Premium wifi'],
+				distance : '400m'
+			}
+		]
 	})
 }
 
