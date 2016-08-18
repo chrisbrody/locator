0e7962e80f5fa65c1d24a78e69962619f35ff356
chrisbrody <chrisbrody@codifyacademy.com>
2016-08-17 20:01:58 -0700

    added all layouts

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,10 +5,12 @@ module.exports.homelist = function(req, res) {
 
 /* GET Location Info */
 module.exports.locationInfo = function(req, res) {
-	res.render('locations-list', { title : 'Location Info' })
+	res.render('location-info', { title : 'Location Info' })
 }
 
 /* GET Add Review page */
 module.exports.addReview = function(req, res) {
-	res.render('index', { title : 'Add Review' })
+	res.render('location-review-form', { title : 'Add Review' })
 }
+
+
